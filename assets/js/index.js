c6f493f58a666c659d1a63e5cc384ff4d249c2ca
abel mengich <kipyegoabel2308@gmail.com>
2024-04-11 20:59:23 -0400

    updating work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 const API_BASE = "https://api.spoonacular.com/recipes"; 
-const API_KEY = "";
+const API_KEY = "f01c2ae708b445da83e044d46ad551c7";
 
 
 let ingredients ;
@@ -10,7 +10,8 @@ let ingredients ;
  /* Starting with default given value */
 async function findByIngdredients(){
     
-    let ingredients = "apple,flour,sugar";
+    let ingredients = "apples,+flour,+sugar";
+    
     const response = await fetch(`${API_BASE}/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${API_KEY}`);
     listRecipes = await response.json();
     console.log(listRecipes);
