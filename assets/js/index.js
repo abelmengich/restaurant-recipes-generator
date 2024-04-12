f3d53cfac72b34acb829c5d5e6822efdf2b185a5
abel mengich <kipyegoabel2308@gmail.com>
2024-04-12 06:03:08 -0400

    upto date

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,13 +11,22 @@ let ingredients ;
 async function findByIngdredients(){
     
     let ingredients = "apples,+flour,+sugar";
-    
+
     const response = await fetch(`${API_BASE}/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${API_KEY}`);
     listRecipes = await response.json();
-    console.log(listRecipes);
+
+
+    listRecipes.map(
+        function(element){
+            
+            document.getElementById("recipes-container").innerHTML += `<div class="recipe-card"><a href="#"><h2>"${element.title}"</h2><img src="${element.image}"/></a></div>`;
+            console.log(element);
+        }
+    );
 }
 
 findByIngdredients();
+console.log("hello world");
 
 });
 
